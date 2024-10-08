44f3d4fafe4b7395a559831918e6c2c82a880252
armanrohaj <arman.rohaj3@gmail.com>
2024-10-08 11:18:43 -0400

    Student Class is added

--- src/main/java/com/codeWithDurgesh/Main.java
+++ src/main/java/com/codeWithDurgesh/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("this the first jenkins online repository");
         System.out.println("A new file is added");
+
+        Student student = new Student();
+        student.sayHello();
     }
 }
---
+++ src/main/java/com/codeWithDurgesh/Student.java
@@ -0,0 +1,12 @@
+package com.codeWithDurgesh;
+
+public class Student {
+
+    public Student(){
+        System.out.println("Creating the Student object") ;
+    }
+
+    public void sayHello(){
+        System.out.println("Hello World") ;
+    }
+}
